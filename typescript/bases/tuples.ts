90bbccd55ec4bb68b2fd988050286389061e385c
MyLittleViruzbx1 <mylittleviruz@gmail.com>
2024-08-20 16:29:46 -0600

    typescript tuplas and functions

---
+++ typescript/bases/tuples.ts
@@ -0,0 +1,12 @@
+(() =>{
+    const hero: [string, number, boolean] = ['Dr. Simi', 100, true]
+
+
+    hero[0] = 'Iron man';
+    hero[1] = 75;
+    hero[2] = false;
+    console.log(hero)
+
+
+
+})()
---
+++ typescript/funciones/function.js
@@ -0,0 +1,10 @@
+(function () {
+    var hero = 'Flash';
+    function returnName() {
+        return hero;
+    }
+    var acttionTurbo = function () {
+        return 'Max steel!';
+    };
+    console.log(typeof (acttionTurbo));
+})();
